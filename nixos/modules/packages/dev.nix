6546135e327ee76e15a268c2e601626381cf6504
RohaanKhawaja <rohaankhawaja@outlook.com>
2026-06-22 00:59:56 +0100

    Package Additions

--- nixos/modules/packages/dev.nix
+++ nixos/modules/packages/dev.nix
@@ -1,8 +1,8 @@
 # Rohaan's NixOS Config - Developer Packages
 
-{ config, pkgs, ... }: 
+{ config, pkgs, ... }:
 
-# List of Python Packages + Python 3 
+# List of Python Packages + Python 3
 let
   myPython = pkgs.python3.withPackages (ps: with ps; [
     pip                       # Python Package Manager
@@ -14,21 +14,26 @@ in
 {
 
   environment.systemPackages = with pkgs; [
-    # Compilers 
+    # Compilers
     gcc                       # C/C++ Compiler
-    llvmPackages.clang        # C/C++ Compiler (Clang/LLVM) 
+    llvmPackages.clang        # C/C++ Compiler (Clang/LLVM)
     myPython                  # Python3 + Above packages
-    texliveFull               # LaTeX  
-    openjdk17                 # Java (JDK) 
+    texliveFull               # LaTeX
+    openjdk17                 # Java (JDK)
     zig                       # Zig Compiler
-    
+
     # Build Systems
     cmake                     # Cross-platform Build System
-    meson                     # Build system 
-    ninja                     # Build system backend 
+    meson                     # Build system
+    ninja                     # Build system backend
 
     # Alternatives
     octave                    # GNU Octave (MATLAB-like)
+
+    # Arduino
+    arduino-ide               # Arduino IDE
+    arduino-cli               # Arduino CLI
+
   ];
 
 }
