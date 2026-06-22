23ff40970cca71c63e66316fd1673a0b61e401e7
RohaanKhawaja <rohaankhawaja@outlook.com>
2026-06-22 22:42:26 +0100

    hypr restructure

---
+++ hypr/.config/hypr/modules/autostart.lua
@@ -0,0 +1,16 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+
+hl.on("hyprland.start", function()
+  local apps = {
+    "waybar",
+    "hyprpaper",
+    "hypridle",
+    "hyprcursor",
+  }
+  hl.exec_cmd(table.concat(apps, " & "))
+
+  hl.exec_cmd("wl-paste --type text --watch cliphist store")
+  hl.exec_cmd("wl-paste --type image --watch cliphist store")
+end)
