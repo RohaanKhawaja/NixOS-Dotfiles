3044eccf3b379dbe672c2ea8ce6cc51b871dae69
RohaanKhawaja <rohaankhawaja@outlook.com>
2026-07-09 16:41:25 +0100

    Acer Laptop Transition

--- hypr/.config/hypr/modules/monitors.lua
+++ hypr/.config/hypr/modules/monitors.lua
@@ -4,25 +4,9 @@
 
 -- Primary
 hl.monitor({
-    output   = "DP-9",
+    output   = "eDP-1",
     mode     = "preferred",
     position = "0x0",
-    scale    = "1.0",
+    scale    = "1.67",
 })
 
--- Secondary
-hl.monitor({
-    output   = "DP-11",
-    mode     = "preferred",
-    position = "0x1080",
-    scale    = "1.0",
-})
-
--- Built-In
-hl.monitor({
-    output   = "eDP-1",
-    mode     = "1600x2560@144",
-    position = "-1600x1080",
-    scale    = "1.6",
-    transform = 1,
-})
